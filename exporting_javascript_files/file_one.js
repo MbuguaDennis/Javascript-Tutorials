36f203ce45242c1b6b0fb888c7c6585a518ce873
MbuguaDennis <dmbugua.developer@gmail.com>
2023-05-09 16:54:47 +0300

    exporting modules

---
+++ exporting_javascript_files/file_one.js
@@ -0,0 +1,28 @@
+const uppercaseString = (string) => {
+    return string.toUpperCase();
+  }
+  
+  const lowercaseString = (string) => {
+    return string.toLowerCase()
+  }
+
+   export const myVariable = (arr) => {
+    let product =1;
+    for(let i=0; i<=arr.length;i++){
+        // product.push(arr[i]);
+        product *=arr[i];
+        
+    }
+    return product;
+   }
+
+  const addition = (x,y) => x+y;
+//   This course exports  functions to  be used in another file.
+  export { uppercaseString,lowercaseString, addition};
+
+
+
+
+
+
+  
---
+++ exporting_javascript_files/file_two.js
@@ -0,0 +1,17 @@
+
+import * as myObject from "./file_one.js";
+
+
+
+console.log(myObject.uppercaseString("Dennis"));
+console.log(myObject.lowercaseString("Mbugua"));
+
+console.log(myObject.addition(99,100));
+
+// Use asterisk to import all files using *
+console.log(myObject.myVariable(["20,30,5"]));
+
+const newUser = anotherUser("Dennis");
+console.log(myObject.newUser);
+
+
